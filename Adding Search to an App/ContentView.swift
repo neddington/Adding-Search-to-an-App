7372ad7964c9c0780c805d93227a3330795b799d
neddington <neddington@cascadia.edu>
2023-11-08 16:16:24 -0800

    FIn

--- Adding Search to an App/ContentView.swift
+++ Adding Search to an App/ContentView.swift
@@ -7,17 +7,76 @@
 
 import SwiftUI
 
+struct Movie: Identifiable {
+    var id = UUID()
+    var title: String
+}
+
 struct ContentView: View {
+    @State private var searchText = ""
+    @State private var movies: [Movie] = [
+        Movie(title: "Inception"),
+        Movie(title: "The Shawshank Redemption"),
+        Movie(title: "The Godfather"),
+        Movie(title: "Pulp Fiction"),
+        Movie(title: "Fight Club"),
+        Movie(title: "The Matrix"),
+        Movie(title: "Forrest Gump"),
+        Movie(title: "Gladiator"),
+        Movie(title: "The Dark Knight"),
+        Movie(title: "Schindler's List")
+    ]
+    
+    @State private var initialSuggestions: [String] = []
+    @State private var showSuggestions = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                TextField("Search for a movie", text: $searchText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                    .onTapGesture {
+                        
+                    }
+                    .onSubmit {
+                        showSuggestions = false
+                    }
+                    .foregroundColor(showSuggestions ? .blue : .primary) // Change text color
+                
+                if showSuggestions {
+                    ForEach(initialSuggestions, id: \.self) { suggestion in
+                        Button(action: {
+                            searchText = suggestion
+                            showSuggestions = false
+                        }) {
+                            Text(suggestion + " - Suggestion")
+                        }
+                        .foregroundColor(.blue) // Make suggestions blue
+                    }
+                }
+                
+                ForEach(filteredMovies, id: \.id) { movie in
+                    Text(movie.title)
+                }
+            }
+            .navigationBarTitle("Movie List")
         }
-        .padding()
+        .onAppear {
+            initialSuggestions = movies.shuffled().prefix(3).map { $0.title }
+            showSuggestions = true
+        }
+    }
+    
+    var filteredMovies: [Movie] {
+        if searchText.isEmpty {
+            return movies
+        } else {
+            return movies.filter { $0.title.lowercased().contains(searchText.lowercased()) }
+        }
     }
 }
+
 
 #Preview {
     ContentView()
