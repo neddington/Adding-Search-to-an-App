a327a6c115751f6b2a8d8eb870d53d4176ab274b
neddington <neddington@cascadia.edu>
2023-11-08 16:02:14 -0800

    Initial Commit

---
+++ Adding Search to an App/Adding_Search_to_an_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adding_Search_to_an_AppApp.swift
+//  Adding Search to an App
+//
+//  Created by Eddington, Nick on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Adding_Search_to_an_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adding Search to an App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Adding Search to an App
+//
+//  Created by Eddington, Nick on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
